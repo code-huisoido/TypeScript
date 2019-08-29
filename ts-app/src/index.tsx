831617b4c6aaadc2ecdc30d579fbba0946fdd6bf
huisoido <584331125@qq.com>
2019-08-29 18:13:08 +0800

    some demo

---
+++ ts-app/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import Hello from './components/Hello';
+
+ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={3} />, document.getElementById('root') as HTMLElement);
+registerServiceWorker();
